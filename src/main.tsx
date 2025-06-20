66940c57a0c4fe4a2248abbff47a31e056cd8611
Danilo Ponce <viazenapp@gmail.com>
2025-06-20 02:57:09 -0300

    fix: agrego StrictMode y React para corregir error en producción

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ErrorBoundary } from './components/ErrorBoundary.tsx'
 import './index.css'
@@ -9,6 +9,5 @@ createRoot(document.getElementById('root')!).render(
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
-  </StrictMode>,
+  </StrictMode>
 )
-
